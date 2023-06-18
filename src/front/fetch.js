c7607f4d3a2d9d26ece22680b5920e0c8b2d30a1
Sylvain MARION <sylvain.marion.ext@ac-grenoble.fr>
2023-06-18 11:00:11 +0200

    Added tooltip with engravings used

--- src/front/fetch.js
+++ src/front/fetch.js
@@ -107,22 +107,34 @@ function handleShorthandEngravingNames(engraving_name) {
 
 function handleResults(builds, data) {
     let html = "";
+    const engravingMap = new Map();
     if (builds.length == 0) {
         html = "No build for this combination of stat/engraving";
     } else {
         html = "<div class='build-container'>\n";
+        let i = 0;
         for (let build of builds) {
+            i++;
             let isSubstatOnly = isOnlySubstat(build, data.stats);
             let metaEngravings = isMetaEngravingsOnly(build, data.engravings);
             let statsPart = [];
             for (stat of build.stats) {
                 statsPart.push(`<span class="stat"><span class="stat-name">${stat.name}</span> <span class="stat-importance">${stat.importance}</span></span>`);
             }
-            html += `<div class='build${isSubstatOnly ? " substat-only" : ""}${metaEngravings ? " meta-engravings" : ""}'><span class="build-name">${build.name}</span> (${statsPart.join(", ")})</div>\n`;
+            engravingMap.set(i, `<div class="meta-engraving-list">Meta : ${buildMetaEngravingList(build.engravings)}</div><div class="off-meta-engraving-list">Off-meta : ${buildOffMetaEngravingList(build.engravings)}</div>`);
+            html += `<div id='build-${i}' class='build${isSubstatOnly ? " substat-only" : ""}${metaEngravings ? " meta-engravings" : ""}'><span class="build-name">${build.name}</span> (${statsPart.join(", ")})</div>\n`;
         }
         html += "</div>";
     }
     document.getElementById("results").innerHTML = "<h4>Results : </h4>" + html;
+    for (let [id, tooltipContent] of engravingMap) {
+        tippy(`#build-${id} .build-name`, {
+            content: tooltipContent,
+            allowHTML: true,
+            maxWidth: 'none'
+        });
+    }
+    
 }
 
 function isOnlySubstat(build, stats_wanted) {
@@ -165,4 +177,23 @@ function isMetaEngravingForBuild(build, engraving_wanted) {
         }
     }
     return false;
+}
+
+function buildMetaEngravingList(engravings) {
+    return buildEngravingList(engravings, "meta");
+}
+
+function buildOffMetaEngravingList(engravings) {
+    return buildEngravingList(engravings, "off-meta");
+}
+
+function buildEngravingList(engravings, importance) {
+    let engravingsToKeep = [];
+    for(let engraving of engravings) {
+        if (engraving.importance === importance) {
+            engravingsToKeep.push(`<span class="engraving">${engraving.name}</span>`);
+        }
+    }
+
+    return engravingsToKeep.join(", ");
 }
